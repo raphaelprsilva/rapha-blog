4bc027096c95bcde1dd47506324bd4f92dec587f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-01 10:10:35 -0300

    Add Profile component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import Profile from './Profile/index';
 
 const Layout = ({ children }) => {
   return (
-    <main>{children}</main>
+    <>
+      <aside>
+        <Profile />
+      </aside>
+      <main>{children}</main>
+    </>
   )
 }
 
