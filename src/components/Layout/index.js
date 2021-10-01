d426597c4641946b19097044e912acad1efb3030
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-01 16:46:35 -0300

    Add Menu Bar Component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import GlobalStyles from '../../styles/global';
 import * as S from './styled';
 import Sidebar from '../Sidebar';
+import MenuBar from '../MenuBar/index';
 
 const Layout = ({ children }) => {
   return (
@@ -13,6 +14,7 @@ const Layout = ({ children }) => {
         <Sidebar />
       </aside>
       <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
     </S.LayoutWrapper>
   );
 };
