ba2d94b42d9008bacd81df24aa24a478f729a79c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-01 15:19:18 -0300

    Add SocialLinks Component

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from '@styled-icons/boxicons-logos/Github';
+import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin';
+import { Youtube } from '@styled-icons/boxicons-logos/Youtube';
+import { Instagram } from '@styled-icons/boxicons-logos/Instagram';
+
+const Icons = {
+  Github,
+  Linkedin,
+  Youtube,
+  Instagram,
+};
+
+export default Icons;
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/raphaelprsilva',
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://linkedin.com/raphaelprsilva',
+  },
+  {
+    label: 'Youtube',
+    url: 'https://www.youtube.com/channel/UCQM3IFf1LNcWeRdo3BWDbUQ',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://www.instagram.com/prsraphael/',
+  },
+];
+
+export default links;
