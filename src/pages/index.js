28013376a36ffe1c69529fe29a3c6a8ad1906eac
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-01 18:07:05 -0300

    Pass GraphQL data to IndexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,58 @@
-import * as React from 'react';
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import Seo from '../components/seo';
 import PostItem from '../components/PostItem/index';
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <PostItem
-      slug="/about/"
-      category="Misc"
-      date="30 de Julho de 2019"
-      timeToRead="5"
-      title="Diga não ao Medium: tenha sua própria plataforma"
-      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
-    />
-  </Layout>
-);
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+            wordCount {
+              words
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const postList = allMarkdownRemark.edges;
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+          },
+        }) => (
+          <PostItem
+            slug="/about/"
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  );
+};
 
 export default IndexPage;
