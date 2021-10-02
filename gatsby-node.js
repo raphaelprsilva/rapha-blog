86428fb9f5312abce497ba327ab4dcf505d870b4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-02 08:42:49 -0300

    Add onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const { createFilePath } = require('gatsby-source-filesystem');
 
-// You can delete this file if you're not using it
+// To add the slug field to each post
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  // you only want to operate on `Mdx` nodes. If you had content from a
+  // remote CMS you could also check to see if the parent node was a
+  // `File` node here
+  if (node.internal.type === 'Mdx') {
+    const slug = createFilePath({ node, getNode });
+
+    createNodeField({
+      // Name of the field you are adding
+      name: 'slug',
+      // Individual MDX node
+      node,
+      // Generated value based on filepath with "blog" prefix. you
+      // don't need a separating "/" before the value because
+      // createFilePath returns a path with the leading "/".
+      value: `/${slug.slice(12)}`,
+    });
+  }
+};
