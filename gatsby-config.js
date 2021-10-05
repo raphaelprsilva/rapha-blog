400adf9d37100dbdb2724b5cb06e2f71c41c9c01
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-05 10:48:16 -0300

    Configure searchable attributes and custom ranking from Algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const queries = require('./src/utils/algolia_queries.js');
+
 module.exports = {
   siteMetadata: {
     title: `Raphael Silva`,
@@ -61,6 +65,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
+        queries,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true, // default: false
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
