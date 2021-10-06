85f75717625f7116215a8e3985f765ffb0671734
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-06 19:47:29 -0300

    Fix bug

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -6,28 +6,24 @@ import { InstantSearch, SearchBox, Hits, Stats } from 'react-instantsearch-dom';
 import * as S from './styled';
 import Hit from './Hit';
 
-const algolia = {
-  appId: process.env.GATSBY_ALGOLIA_APP_ID,
-  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+const Search = ({ algolia }) => {
+  const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey);
+
+  return (
+    <S.SearchWrapper>
+      <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+        <SearchBox autoFocus translations={{ placeholder: 'Pesquisar...' }} />
+        <Stats
+          translations={{
+            stats(nbHits, timeSpentMs) {
+              return `${nbHits} resultados encontrados em ${timeSpentMs}ms`;
+            },
+          }}
+        />
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
+    </S.SearchWrapper>
+  );
 };
-
-const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey);
-
-const Search = () => (
-  <S.SearchWrapper>
-    <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
-      <SearchBox autoFocus translations={{ placeholder: 'Pesquisar...' }} />
-      <Stats
-        translations={{
-          stats(nbHits, timeSpentMs) {
-            return `${nbHits} resultados encontrados em ${timeSpentMs}ms`;
-          },
-        }}
-      />
-      <Hits hitComponent={Hit} />
-    </InstantSearch>
-  </S.SearchWrapper>
-);
 
 export default Search;
