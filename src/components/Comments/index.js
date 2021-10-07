dfd91223c454f5020c40830ecc017fe6e16d7971
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-07 12:51:31 -0300

    Change url name

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,8 +5,8 @@ import ReactDisqusComments from 'react-disqus-comments';
 import * as S from './styled';
 
 const Comments = ({ url, title }) => {
-  const completeUrl = `http://localhost:8000${url}`;
-  console.log(completeUrl);
+  const completeUrl = `https://www.elraphael.com.br${url}`;
+
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
