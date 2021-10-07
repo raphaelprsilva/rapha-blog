3b166f407b9cfdd500290cc1751e525d9c287d00
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-07 12:57:56 -0300

    Change shortname property value

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="prsraphael"
+        shortname="elraphael-com-br"
         identifier={completeUrl}
         title={title}
         url={completeUrl}
