c9ad77038803b9ea32f6fe317df530a43dd69032
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-10-07 11:55:32 -0300

    Change side bar width

--- src/components/Sidebar/styled.js
+++ src/components/Sidebar/styled.js
@@ -11,7 +11,7 @@ export const SidebarWrapper = styled.aside`
   position: fixed;
   padding: 2rem;
   text-align: center;
-  width: 17rem;
+  width: 16rem;
 
   ${media.lessThan('large')`
     align-items: flex-start;
