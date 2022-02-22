b9d6a84c4895eb49a1a531784c9040c6a265e3f2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-22 14:30:21 -0300

    change search placeholder

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -12,7 +12,7 @@ const Search = ({ algolia }) => {
   return (
     <S.SearchWrapper>
       <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
-        <SearchBox autoFocus translations={{ placeholder: 'Pesquisar...' }} />
+        <SearchBox autoFocus translations={{ placeholder: 'Pesquisar aqui...' }} />
         <Stats
           translations={{
             stats(nbHits, timeSpentMs) {
