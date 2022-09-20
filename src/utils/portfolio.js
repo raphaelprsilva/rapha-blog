9a44cb8464505a7190198e5ba9a70fd76c451868
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-09-20 13:00:36 -0300

    feat: add stacks

--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -1,6 +1,7 @@
 const vanillaJavascript = [
   {
     projectName: 'Madruga Shopping Cart',
+    stacks: ['HTML', 'CSS', 'js', 'Jest'],
     projectDescription:
       'Projeto na qual a pessoa usuária pode adicionar items ao carrinho e a soma dos valores é computada',
     siteLink: 'https://madruga-shopping.elraphael.com.br/',
@@ -8,6 +9,7 @@ const vanillaJavascript = [
   },
   {
     projectName: 'Trybewarts',
+    stacks: ['HTML', 'CSS', 'js'],
     projectDescription:
       'Projeto simulando uma página de login e um formulário de cadastro de usuário',
     siteLink: 'https://trybe-warts.elraphael.com.br/',
@@ -15,6 +17,7 @@ const vanillaJavascript = [
   },
   {
     projectName: 'Breaking Bad - Jogo da Memória',
+    stacks: ['HTML', 'CSS', 'js'],
     projectDescription: 'Jogo de memória em homenagem à série Breaking Bad',
     siteLink: 'https://breaking-bad.elraphael.com.br/',
     githubLink:
@@ -22,6 +25,7 @@ const vanillaJavascript = [
   },
   {
     projectName: 'Pixels Art Project',
+    stacks: ['HTML', 'CSS', 'js'],
     projectDescription:
       'Projeto de arte em pixels, onde a pessoa usuária consegue "pintar", selecionando as cores e o tamanho da tela.',
     siteLink: 'https://pixels-art.elraphael.com.br/',
@@ -29,6 +33,7 @@ const vanillaJavascript = [
   },
   {
     projectName: 'Meme Generator',
+    stacks: ['HTML', 'CSS', 'js'],
     projectDescription:
       'Projeto de um simples gerador de memes, na qual é possível carregar uma imagem ou selecioná-la.',
     siteLink: 'https://meme-generator.elraphael.com.br/',
@@ -36,6 +41,7 @@ const vanillaJavascript = [
   },
   {
     projectName: 'Todo List',
+    stacks: ['HTML', 'CSS', 'js'],
     projectDescription:
       'Projeto de to do list na qual é possível incluir items, remover items e filtrar items',
     siteLink: 'https://todo-list.elraphael.com.br/',
@@ -43,6 +49,7 @@ const vanillaJavascript = [
   },
   {
     projectName: 'Weather App',
+    stacks: ['HTML', 'CSS', 'js'],
     projectDescription:
       'Projeto com informações sobre o clima, onde a pessoa usuária digita o nome da cidade e obtém as informações.',
     siteLink: 'https://weather-app.elraphael.com.br/',
@@ -53,6 +60,7 @@ const vanillaJavascript = [
 const reactProjects = [
   {
     projectName: 'Madruga Tunes',
+    stacks: ['React', 'styled-components', 'styled-icons'],
     projectDescription:
       'Projeto de música, na qual o usuário pode pesquisar por cantores, salvar uma playlist e escutar um trecho da música favorita"',
     siteLink: 'https://www.madruga-tunes.elraphael.com.br/',
@@ -60,6 +68,7 @@ const reactProjects = [
   },
   {
     projectName: 'Solar System',
+    stacks: ['React', 'styled-components'],
     projectDescription:
       'Um simples projeto feito em React, na qual é possível visualizar os planetas do sistema solar e as missões realizadas ao espaço',
     siteLink: 'https://solar-system.elraphael.com.br/',
@@ -67,6 +76,7 @@ const reactProjects = [
   },
   {
     projectName: 'Jogo Trunfo - Heroínas Brasileiras',
+    stacks: ['React', 'styled-components', 'react-modal'],
     projectDescription:
       'Projeto baseado no jogo Trunfo, com a temática "Heroínas Brasileiras - Mulheres que fizeram e fazem história no Brasil"',
     siteLink: 'https://www.heroinas-brasileiras.elraphael.com.br/',
